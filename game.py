a9aa767708ced7de82b0f57af820f5820a905089
Naklate <robiswoje6@gmail.com>
2024-07-10 03:26:40 +0100

    Addet characters

--- game.py
+++ game.py
@@ -28,6 +28,34 @@ def draw_bg():
 def draw_panel():
     screen.blit(panel_img, (0, screen_height - bottom_panel))
 
+#fighter class
+class Fighter():
+    def __init__(self, x, y, name, max_hp, strenght, potions):
+       self.name = name
+       self.max_hp = max_hp 
+       self.hp = max_hp
+       self.strenght = strenght
+       self.start_potions = potions
+       self.alive = True
+       img = pygame.image.load(f'img/{self.name}/Idle/0.png')
+       self.image = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() *3))
+       self.rect = self.image.get_rect()
+       self.rect.center = (x, y)
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+knight = Fighter(200, 260, 'Knight', 300, 10, 3)
+bandit1 = Fighter(550, 270, 'Bandit', 20, 6, 1)
+bandit2 = Fighter(700, 270, 'Bandit', 20, 6, 1)
+
+bandit_list = []
+bandit_list.append(bandit1)
+bandit_list.append(bandit2)
+
+
+
+
 run = True
 while run:
 
@@ -38,6 +66,13 @@ while run:
 
     #draw panel
     draw_panel()
+
+    #draw fighters
+    knight.draw()
+    for bandit in bandit_list:
+        bandit.draw()
+
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
